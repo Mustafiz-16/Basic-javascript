1601bd288f070c04e7fece3db00d316306faaf5b
mustafiz <mustafizurrahaman362@gmail.com>
2025-06-29 22:42:46 +0600

    Add helloworld.js and variable_and_naming.js

---
+++ javascript/helloworld.js
@@ -0,0 +1,14 @@
+console.log("Hello world");
+console.log("i am mustafiz");
+console.log("i am mustafiz")
+
+//differece between number and text 
+
+console.log(40)
+console.log("40")
+console.log(3+4)
+console.log("3+4")
+
+console.log(40/2);
+console.log(40*2);
+console.log(40-2);
