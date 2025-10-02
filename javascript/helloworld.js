b585da317962da6e9352d9d98fc4d40cd9d07148
mustafiz <mustafizurrahaman362@gmail.com>
2025-10-02 15:47:12 +0600

    Moved all JS files into javascript folder

--- javascript/helloworld.js
+++ javascript/helloworld.js
@@ -5,8 +5,8 @@ console.log("i am mustafiz")
 //differece between number and text 
 
 console.log(40)
-console.log("40")
-console.log(3+4)
+console.log("40") //string
+console.log(3+4) //number
 console.log("3+4")
 
 console.log(40/2);
